2ee9f2b663e65adb4e35a1e3abca64be4d48e01c
jhung-mililani <115854825+jhung-mililani@users.noreply.github.com>
2022-11-05 00:32:54 -1000

    undo changes to stopServices.bat
    idk how to use git and pull requests so this is a manual undo.

--- stopServices.bat
+++ stopServices.bat
@@ -13,31 +13,16 @@ set servicesStopList=PlugPlay Spooler
 set servicesStartList=
 set windowsUpdateServices=wuauserv
 
-:: This function uses the Service Control Manager to stop the service that is passed through it as a parameter. 
 :stopService 
-sc stop %~1
-if ERRORLEVEL 1 echo Unable to stop %~1. Check to see if it is already stopped. 
-   pause 
-exit /b 0 
 
-:: This function uses the Service Control Manager to disable the service that is passed through it as a parameter. 
-:disableService 
-sc config %~1 start=disabled
-if ERRORLEVEL 1 echo Unable to disable %~1. Check to see if it is already disabled. 
-   pause 
-exit /b 0 
 
-:: This function uses the Service Control Manager to start the service that is passed through it as a parameter. 
-:startService 
-sc start %~1 
-if ERRORLEVEL 1 echo Unable to start %~1. Check to see if it is already started. 
+for %%i in (%servicesList%) do (
+   sc stop %%i 
+   if ERRORLEVEL 1 echo Unable to stop %%i. Check to see if it is already stopped. 
    pause 
-exit /b 0 
-
-:: This loop iterates over the given array and stops and disables those services. TODO: make array mutable by prompting user. 
-for %%i in (%servicesStopList%) do (
-   call :stopService %servicesStopList%[%%i] 
-   call :disableService %servicesStopList%[%%i]
+   sc config %%i start=disabled 
+   if ERRORLEVEL 1 echo Unable to disable %%i. Check to see if it is already disabled. 
+   pause 
 ) 
 
 cmd /k 
