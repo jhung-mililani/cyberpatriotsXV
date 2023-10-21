8bc64d2a2f7ebb45e7bd85aa17936473a2bb5d76
jhung-mililani <115854825+jhung-mililani@users.noreply.github.com>
2023-10-20 18:58:44 -1000

    PSAvoidUsingWriteHost

--- secureUsers.ps1
+++ secureUsers.ps1
@@ -5,16 +5,16 @@ $allowedUsers = @(Get-Content $allowedUsersPath)
 $unauthUserPath = "$($env:USERPROFILE)\Desktop\unauthUsers.txt"
 $unauthUsers = Get-LocalUser | Where-Object { $allowedUsers -notcontains $_.Name }
 
-Write-Host "Warning: Ensure you've created the file allowedUsers.txt at [$allowedUsersPath], case-sensitive, with the users from both the administrator and user sections of the readme before running this script." -ForegroundColor Yellow
+Write-Output "Warning: Ensure you've created the file allowedUsers.txt at [$allowedUsersPath], case-sensitive, with the users from both the administrator and user sections of the readme before running this script."
 Read-Host -Prompt "Press any key to continue"
 
 try
 {
     $unauthUsers | Select-Object -ExpandProperty Name | Out-File -FilePath $unauthUserPath
     $unauthUsers | Remove-LocalUser -Confirm -ErrorAction Inquire
-    Write-Host "Done removing unauthorized users, log can be found at [$unauthUserPath]" -ForegroundColor Green
+    Write-Output "Done removing unauthorized users, log can be found at [$unauthUserPath]"
     Read-Host -Prompt "Press any key to exit"
 }
 catch {
-    Write-Host "An error occurred: $($_.Exception.Message)" -ForegroundColor Red
+    Write-Output "An error occurred: $($_.Exception.Message)"
 }
