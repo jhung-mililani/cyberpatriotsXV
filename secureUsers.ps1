973bd14eaac2af2df0f2ed011d528e14f74c73f4
jhung <115854825+jhung-mililani@users.noreply.github.com>
2023-11-03 15:57:56 -1000

    add default accounts

--- secureUsers.ps1
+++ secureUsers.ps1
@@ -3,7 +3,8 @@
 $allowedUsersPath = "$($env:USERPROFILE)\Desktop\allowedUsers.txt"
 $allowedUsers = @(Get-Content $allowedUsersPath)
 $unauthUserPath = "$($env:USERPROFILE)\Desktop\unauthUsers.txt"
-$unauthUsers = Get-LocalUser | Where-Object { $allowedUsers -notcontains $_.Name }
+$defaultAccounts = @("Administrator", "DefaultAccount", "Guest", "WDAGUtilityAccount")
+$unauthUsers = Get-LocalUser | Where-Object { $allowedUsers -notcontains $_.Name } | Where-Object { $defaultAccounts -notcontains $_.Name}
 
 Write-Output "Warning: Ensure you've created the file allowedUsers.txt at [$allowedUsersPath], case-sensitive, with the users from both the administrator and user sections of the readme before running this script."
 Read-Host -Prompt "Press any key to continue"
@@ -17,4 +18,4 @@ try
 }
 catch {
     Write-Output "An error occurred: $($_.Exception.Message)"
-}
+}r
